9806d24819715e6f1a605b2ece8eedda12866583
Rohit Savant <savantrohit8@gmail.com>
2018-05-20 19:53:20 +0530

    Impelemented click listener on Menu

--- app/src/main/java/com/narmware/realpic/Main2Activity.java
+++ app/src/main/java/com/narmware/realpic/Main2Activity.java
@@ -3,9 +3,11 @@ package com.narmware.realpic;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.narmware.realpic.support.Support;
 import com.yarolegovich.slidingrootnav.SlidingRootNav;
@@ -15,7 +17,18 @@ import com.yarolegovich.slidingrootnav.callback.DragStateListener;
 
 public class Main2Activity extends AppCompatActivity implements DragStateListener {
     private SlidingRootNav slidingRootNav;
+    private FragmentManager mFragmentManager;
+    private Button test;
 
+    private void init() {
+      test = findViewById(R.id.test);
+      test.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Support.mt("Clicked!", Main2Activity.this);
+          }
+      });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +36,6 @@ public class Main2Activity extends AppCompatActivity implements DragStateListene
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         slidingRootNav = new SlidingRootNavBuilder(this)
                 .withToolbarMenuToggle(toolbar)
@@ -41,6 +45,9 @@ public class Main2Activity extends AppCompatActivity implements DragStateListene
                 .withMenuLayout(R.layout.menu_layout)
                 .addDragStateListener(this)
                 .inject();
+        init();
+
+
     }
 
     @Override
@@ -52,4 +59,5 @@ public class Main2Activity extends AppCompatActivity implements DragStateListene
     public void onDragEnd(boolean isMenuOpened) {
         Support.mt("Menu staus : " + isMenuOpened, Main2Activity.this);
     }
+
 }
