966f1b1015b26eb24e4c069b9d2e477189cd1469
Rohit Savant <savantrohit8@gmail.com>
2018-05-20 21:27:51 +0530

    News fetch implemented, made server calls

--- app/src/main/java/com/narmware/realpic/fragments/NewsFragment.java
+++ app/src/main/java/com/narmware/realpic/fragments/NewsFragment.java
@@ -4,11 +4,32 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alexvasilkov.foldablelayout.FoldableListLayout;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 import com.narmware.realpic.R;
+import com.narmware.realpic.activity.HomeActivity;
+import com.narmware.realpic.apdapter.HomeNewsAdapter;
+import com.narmware.realpic.pojo.HomeNews;
+import com.narmware.realpic.pojo.HomePojoResponse;
+import com.narmware.realpic.support.EndPoint;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,8 +48,13 @@ public class NewsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private View mRoot;
+   protected FoldableListLayout mNewsList;
+    ArrayList<HomeNews> homeNewsPojos = new ArrayList<>();
 
     private OnFragmentInteractionListener mListener;
+    private HomeNewsAdapter mHomeNewsAdapter;
+    private RequestQueue mQueue;
 
     public NewsFragment() {
         // Required empty public constructor
@@ -65,7 +91,11 @@ public class NewsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        mRoot = inflater.inflate(R.layout.fragment_news, container, false);
+        mNewsList = mRoot.findViewById(R.id.foldable_list);
+        fetchNews();
+
+        return mRoot;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -80,6 +110,7 @@ public class NewsFragment extends Fragment {
         super.onAttach(context);
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
+
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -105,5 +136,56 @@ public class NewsFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    public void fetchNews()
+    {
+        if (mQueue == null) {
+            mQueue = Volley.newRequestQueue(getContext());
+        }
+
+        String url= EndPoint.NEWS_URL+"?id";
+        Log.e("url",url);
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>(){
+
+
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Log.d("urlr", response.toString());
+
+                        Gson gson=new Gson();
+                        HomePojoResponse pojo1=gson.fromJson(response.toString(),HomePojoResponse.class);
+
+                        final HomeNews[] array=pojo1.getData();
+                        for(HomeNews item:array)
+                        {
+                            // String ImgId=item.getId();
+
+                            homeNewsPojos.add(item);
+                            Log.d("info",item.getId());
+                            Log.d("details ",item.getUrl());
+
+                        }
+                        mHomeNewsAdapter = new HomeNewsAdapter(homeNewsPojos,getActivity());
+                        mNewsList.setAdapter(mHomeNewsAdapter);
+                        mHomeNewsAdapter.notifyDataSetChanged();
+
+                    }
+
+
+                },new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        Log.d("Details", error.toString());
+                    }
+                });
+
+        mQueue.add(jsObjRequest);
+
     }
 }
---
+++ app/src/main/java/com/narmware/realpic/support/EndPoint.java
@@ -0,0 +1,11 @@
+package com.narmware.realpic.support;
+
+/**
+ * Created by Ashwini Palve on 17/05/2018.
+ */
+
+public class EndPoint
+{
+    public static final String BASE_URL="http://www.onlineshopee.co.in/realpic/api/";
+    public static final String NEWS_URL = BASE_URL + "list.php";
+}
